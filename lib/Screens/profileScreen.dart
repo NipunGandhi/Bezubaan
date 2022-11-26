312b738e3aa41509bab3ee072f1f4471e60918eb
NipunGandhi <nipungandhi21@gmail.com>
2022-11-27 01:41:12 +0530

    Profile Screen UI completed TODO - Fetch Data For Post Screen - Fetch Data For Profile Screen - Location work

--- lib/Screens/profileScreen.dart
+++ lib/Screens/profileScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../Widgets/postWidget.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -6,9 +7,65 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Archit"),
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 1,
+              ),
+            ),
+            padding: const EdgeInsets.only(top: 35, left: 20, bottom: 20),
+            child: Row(
+              children: [
+                Column(
+                  children: const [
+                    CircleAvatar(
+                      radius: 50,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Username",
+                    ),
+                  ],
+                ),
+                const SizedBox(width: 25),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 8),
+                    const Text("E-Mail:"),
+                    const SizedBox(height: 8),
+                    const Text("Mobile No:"),
+                    const SizedBox(height: 8),
+                    const Text("Location:"),
+                    const SizedBox(height: 8),
+                    ElevatedButton(
+                        onPressed: () => {
+                              print("Whatsapp Contact"),
+                            },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.green,
+                        ),
+                        child: const Text("Whatsapp")),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(0),
+              children: const [
+                PostWidget(),
+                PostWidget(),
+                PostWidget(),
+                PostWidget(),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
